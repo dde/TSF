03117aa65e891bbf324415ab6c9df40640c10802
Dan Evans <danevans@acm.org>
2020-06-09 19:38:31 -0400

    remove

--- Java/NpxFactory.java
+++
@@ -1,9 +0,0 @@
-package edu.pace.csis.evans.npx;
-
-/**
- * Created by danevans on 3/10/14.
- */
-public interface NpxFactory
-{
-NpxCommon build(String qName, String localName, String uri, Object attrs, NpxGlobal interp);
-}
